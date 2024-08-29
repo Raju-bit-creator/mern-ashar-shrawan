63c8fce9dd89b241a70eaf890146489b23566d3f
Raju <rajutamang3456@gmail.com>
2024-08-29 16:29:07 +0545

    user inserting

--- backend/index.js
+++ backend/index.js
@@ -2,13 +2,15 @@ const express= require('express')
 const dbConnect = require('./db')
 const app = express()
 dbConnect()
-
+//  get post update delete crud
 const port= 5000
 app.use(express.json())
  app.get('/', (req, res)=>{
     res.send('hello nepal')
  })
+
+app.use('/api/auth', require('./routes.js/Auth'))
+
  app.listen(port,()=>{
   console.log(`api is listening on port: ${port}`);
-  
- })
+   })
---
+++ backend/routes.js/Auth.js
@@ -0,0 +1,20 @@
+const express =require('express')
+const User = require('../model/User')
+const router =express.Router()
+
+router.get("/",(req, res)=>{
+    mern={
+        a:"sanjay",
+        b: "roman"
+    }
+    res.send(mern)
+})
+router.post("/createuser", (req, res)=>{
+    console.log(req.body);
+    res.send(req.body)
+    const user= User(req.body)
+    user.save()
+
+    
+})
+module.exports=router
