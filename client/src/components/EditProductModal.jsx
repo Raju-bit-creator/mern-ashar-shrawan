c48502d6dad53648f042342939ff4bf9c375c922
Raju <rajutamang3456@gmail.com>
2024-09-20 16:56:09 +0545

    updatting products

---
+++ client/src/components/EditProductModal.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const EditProductModal = (props) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default EditProductModal
--- client/src/components/Serviceitems.jsx
+++ client/src/components/Serviceitems.jsx
@@ -1,25 +1,45 @@
 
-import React, { useContext, useEffect } from 'react';
-import coffee from '../assets/coffee.jpg';
+import React, { useContext, useEffect, useState } from 'react';
 import blogContext from '../context/blogs/BlogContext';
+import { BsThreeDots } from "react-icons/bs";
+import EditProductModal from './EditProductModal';
 
 const Serviceitems = () => {
   const context = useContext(blogContext);
-  const { state: { cart, products }, dispatch, allProduct ,product } = context;
+  const { state: { cart, products }, dispatch, allProduct, product } = context;
+  const[ menuVisible, setMenuVisible]= useState(false)
+  const[ modalVisible, setModalVisible]= useState(false)
+
+
+  const toogleMenu= ()=>{
+   setMenuVisible(!menuVisible)
+  }
+  
+  const openEditModal= ()=>{
+    setModalVisible(true)
+   }
+   const closeEditModal= ()=>{
+    setModalVisible(false)
+   }
+
+   const saveEdit =(updateData)=>{
+     editProduct(_id , updateData)
+     closeEditModal()
+   }
 
   // Fetch products when the component mounts
   useEffect(() => {
     allProduct();  // Fetch products from backend
-   
+
   }, []);  // Empty dependency array to run once after initial render
 
 
 
-  
+
 
   return (
     <div className='container'>
-     
+
       <h4 className='service-heading my-3'>Our Products</h4>
       <div className='row'>
         {product && product.map((e) => {
@@ -28,7 +48,16 @@ const Serviceitems = () => {
               <div className="card" key={e._id}>
                 <img src={`http://localhost:5000/uploads/${e.images[0]}`} className="card-img-top" alt={e.title} />
                 <div className="card-body">
-                  <h5 className="card-title">{e.title}</h5>
+                  <div className='three-dots'>
+                    <h5 className="card-title">{e.title}</h5>
+                    <BsThreeDots  onClick={toogleMenu}/>
+                    {menuVisible && (
+                      <div className='menu-options'>
+                        <button onClick={openEditModal}>Edit</button>
+                        <button onClick={()=> deleteWork(_id)}>Delete</button>
+                      </div>
+                    )}
+                  </div>
                   <p className="card-text">Rs. {e.price}</p>
                   {/* Check if the product is in the cart */}
                   {cart && cart.some(p => p._id === e._id) ? (
@@ -60,6 +89,12 @@ const Serviceitems = () => {
               </div>
             </div>
           );
+          {modalVisible && (
+            <EditProductModal 
+            product= {props.product}
+            onClose={closeEditModal}
+            onSave= {saveEdit}/> 
+          )}
         })}
       </div>
     </div>
